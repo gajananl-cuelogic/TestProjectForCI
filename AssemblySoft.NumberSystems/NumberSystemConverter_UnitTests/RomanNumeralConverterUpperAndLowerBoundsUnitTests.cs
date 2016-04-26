6f9c39fdf59b49e0e75149dfbb376a2343a95837
gajananl-cuelogic <gajanan.londhe@cuelogic.co.in>
2016-04-26 20:14:28 +0530

    updated

--- AssemblySoft.NumberSystems/NumberSystemConverter_UnitTests/RomanNumeralConverterUpperAndLowerBoundsUnitTests.cs
+++ AssemblySoft.NumberSystems/NumberSystemConverter_UnitTests/RomanNumeralConverterUpperAndLowerBoundsUnitTests.cs
@@ -12,6 +12,7 @@ namespace NumberSystemConverter_UnitTests
         [ExpectedException(typeof (IndexOutOfRangeException))]
         public void Number_Greater_Than_ThreeThousand_Throws_IndexOutOfRangeException_TestMethod()
         {
+            //update on 4/26/2016 at 8:13
             var converter = new RomanNumeralConverter();
             converter.ConvertRomanNumeral(3001);
         }
